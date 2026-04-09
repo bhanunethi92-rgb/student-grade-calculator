4835d3c2c43ed84c3e91f2e10ee9a5bdf00f70be
bhanunethi92-rgb <bhanu.nethi92@gmail.com>
2026-04-09 10:45:28 +0530

    Update grade_calculator.py

--- grade_calculator.py
+++ grade_calculator.py
@@ -13,26 +13,21 @@ def calculate_grade(avg):
     else:
         return "Fail"
 
-# Input
-name = input("Enter student name: ")
+name = "Bhanu"
+mark1 = 85
+mark2 = 78
+mark3 = 92
 
-mark1 = int(input("Enter marks for subject 1: "))
-mark2 = int(input("Enter marks for subject 2: "))
-mark3 = int(input("Enter marks for subject 3: "))
-
-# Calculation
 total = mark1 + mark2 + mark3
 average = total / 3
 grade = calculate_grade(average)
 
-# Pass or Fail
 if average >= 40:
     result = "Pass"
 else:
     result = "Fail"
 
-# Output
-print("\n----- Student Report -----")
+print("----- Student Report -----")
 print("Student Name:", name)
 print("Marks:")
 print("Subject 1:", mark1)
